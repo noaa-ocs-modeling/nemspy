a35138b716fe0fc128d334971c038975bd317b72
Jaime R Calzada <jreniel@gmail.com>
2020-09-12 10:43:51 -0400

    Added comments to example file

--- examples/example_1.py
+++ examples/example_1.py
@@ -2,12 +2,14 @@
 # flake8: noqa
 import sys
 import pathlib
-sys.path.insert(0, str((pathlib.Path(__file__).parent / '..').resolve()))
+sys.path.insert(0, str((pathlib.Path(__file__).parent / '..').resolve())) # This can be removed if setup.py is created
 
 from datetime import timedelta
 
-from nemspy.configuration import NEMSConfiguration as NEMS
-from nesmpy import model
+from nemspy.configuration import NEMSConfiguration as NEMS # I would just call it NEMS
+
+from nesmpy import model # the model namespace is too long having to acces each model from each file.
+
 
 from adcircpy import AdcircRun
 from NWMPy import NWMDriver  # This doesn't exist.
@@ -27,6 +29,9 @@ def main():
     seq.connect('atm', 'hyd')
     seq.connect('wav', 'hyd')
     seq.connect('ocn', 'hyd')
+    # The order of execution is implied by the connectors.
+    # The back end needs to analyze the connectors and write out the execution
+    # order correctly. The user does not need to specify execution order.
     nems.write('nems.configure')
 
 
