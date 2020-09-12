d67820d88a71f8571b6f9efa5fda56eb56c96082
Jaime Calzada <jaime.calzada@noaa.gov>
2020-09-12 10:59:54 -0400

    restructured example variables

--- examples/example_1.py
+++ examples/example_1.py
@@ -18,11 +18,12 @@ from NWMPy import NWMDriver  # This doesn't exist.
 def main():
     args = []
     kwargs = {}
-    ocn = model.ADCIRC(300, AdcircRun(*args, **kwargs))
-    wnd = model.ATMesh('/path/to/wind_data.nc')  # this is always 1 core
-    wav = model.WW3Data('/path/to/wave_data.nc') # this is always 1 core
-    hyd = model.NationalWaterModel(300, NWMDriver(*args, **kwargs))
-    nems = NEMS(ocn=ocn, atm=wnd, wav=wav, hyd=hyd)
+    nems = NEMS(
+        ocn=model.ADCIRC(300, AdcircRun(*args, **kwargs)),
+        atm=model.ATMesh('/path/to/wind_data.nc'),  # this is always 1 core
+        wav=model.WW3Data('/path/to/wave_data.nc'), # this is always 1 core
+        hyd=model.NationalWaterModel(300, NWMDriver(*args, **kwargs))
+        )
     seq = nems.add_sequence(timedelta(hours=1))
     seq.connect('atm', 'ocn')
     seq.connect('wav', 'ocn')
