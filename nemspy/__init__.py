a2b8a17bfb77027d3dffd4f3e5233810ed2c7c47
Zachary.Burnett <zachary.burnett@noaa.gov>
2020-09-15 10:59:05 -0400

    version

--- nemspy/__init__.py
+++ nemspy/__init__.py
@@ -1,2 +1,7 @@
+import dunamai
+
 from .interface import ModelingSystem
 from .model import Model
+
+__version__ = dunamai.get_version('nemspy',
+                                  third_choice=dunamai.Version.from_any_vcs).serialize()
--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
-from dunamai import Version
 from setuptools import config, find_packages, setup
 
 metadata = config.read_configuration('setup.cfg')['metadata']
 
 setup(
     name=metadata['name'],
-    version=Version.from_any_vcs().serialize(),
     author=metadata['author'],
     author_email=metadata['author_email'],
     description=metadata['description'],
@@ -15,7 +13,7 @@ setup(
     url=metadata['url'],
     packages=find_packages(),
     python_requires='>=3.8',
-    setup_requires=['dunamai', 'setuptools>=41.2'],
-    extras_require={'dev': ['coverage', 'nose']},
+    setup_requires=['setuptools>=41.2'],
+    extras_require={'dev': ['coverage', 'dunamai', 'nose']},
     test_suite='nose.collector'
 )
