80d638e97d9bddfc372ee1676418b2f85650053c
Zachary.Burnett <zachary.burnett@noaa.gov>
2020-09-25 08:00:35 -0400

    update example to match readme

--- examples/example_1.py
+++ examples/example_1.py
@@ -1,29 +1,55 @@
-#! /usr/bin/env python
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 from nemspy import ModelingSystem
 from nemspy.model import ADCIRC, AtmosphericMesh, NationalWaterModel, WaveMesh
 
 if __name__ == '__main__':
+    # model run time
+    start_time = datetime(2020, 6, 1)
+    duration = timedelta(days=1)
+
     # returning interval of main run sequence
     interval = timedelta(hours=1)
 
+    # directory to which configuration files should be written
+    output_directory = '~/nems_configuration/'
+
     # model entries
-    ocean_model = ADCIRC(processors=300, verbose=True, DumpFields=False)
-    wave_mesh = WaveMesh()
-    atmospheric_mesh = AtmosphericMesh()
+    atmospheric_mesh = AtmosphericMesh('~/wind_atm_fin_ch_time_vec.nc')
+    wave_mesh = WaveMesh('~/ww3.Constant.20151214_sxy_ike_date.nc')
+    ocean_model = ADCIRC(processors=11, verbose=True, DumpFields=False)
     hydrological_model = NationalWaterModel(processors=769, DebugFlag=0)
 
     # instantiate model system with a specified order of execution
-    nems = ModelingSystem(interval, ocean=ocean_model, wave=wave_mesh,
-                          atmospheric=atmospheric_mesh,
-                          hydrological=hydrological_model)
+    nems = ModelingSystem(start_time, duration, interval,
+                          atm=atmospheric_mesh,
+                          wav=wave_mesh,
+                          ocn=ocean_model,
+                          hyd=hydrological_model)
 
-    # form connections between models using `.connect()`
-    nems.connect('atmospheric', 'ocean')
-    nems.connect('wave', 'ocean')
-    nems.connect('atmospheric', 'hydrological')
-    nems.connect('wave', 'hydrological')
-    nems.connect('ocean', 'hydrological')
+    # form connections between models
+    nems.connect('WAV', 'OCN')
+    nems.connect('ATM', 'HYD')
+    nems.connect('WAV', 'HYD')
 
-    print(nems)
+    # form mediations between models with custom functions
+    nems.mediate('ATM', 'OCN', ['MedPhase_atm_ocn_flux'])
+    nems.mediate('HYD', None)
+    nems.mediate(None, 'OCN', ['MedPhase_prep_ocn'], processors=2)
+
+    # define execution order
+    nems.sequence = [
+        'MED -> OCN',
+        'ATM',
+        'ATM -> MED -> OCN',
+        'WAV -> OCN',
+        'OCN',
+        'WAV',
+        'ATM -> HYD',
+        'WAV -> HYD',
+        'HYD',
+        'HYD -> MED'
+    ]
+
+    # write configuration files to the given directory
+    nems.write(output_directory, overwrite=True)
