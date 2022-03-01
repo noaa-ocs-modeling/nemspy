5852f2d3960792611e9e324291f5bcaf186f01d4
zacharyburnett <zachary.r.burnett@gmail.com>
2022-03-01 11:26:36 -0500

    update build system

--- setup.py
+++ setup.py
@@ -14,24 +14,18 @@ try:
         subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'dunamai'])
         from dunamai import Version
 
-    version = Version.from_any_vcs().serialize()
+    __version__ = Version.from_any_vcs().serialize()
 except RuntimeError as error:
     logging.exception(error)
-    version = '0.0.0'
+    __version__ = '0.0.0'
 
-logging.info(f'using version {version}')
+logging.info(f'using version {__version__}')
 
 metadata = config.read_configuration('setup.cfg')['metadata']
 
 setup(
-    name=metadata['name'],
-    version=Version.from_any_vcs().serialize(),
-    author=metadata['author'],
-    author_email=metadata['author_email'],
-    description=metadata['description'],
-    long_description=metadata['long_description'],
-    long_description_content_type='text/markdown',
-    url=metadata['url'],
+    **metadata['name'],
+    version=__version__,
     packages=find_packages(),
     python_requires='>=3.6',
     setup_requires=['dunamai', 'setuptools>=41.2'],
